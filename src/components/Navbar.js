f90967d0920fb988be49032a46c1fc2ac1a397e9
SebastianFriedersen <sebastian.friedersen@gmail.com>
2020-04-15 17:50:09 +0200

    initial commit and navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,15 +1,22 @@
-import React from 'react'
-import { Link } from 'gatsby'
-import github from '../img/github-icon.svg'
-import logo from '../img/logo.svg'
+import React from "react";
+import { Link } from "gatsby";
+
+import logo from "../img/logo_MAZ.svg";
+import audiLogo from "../img/logos_hersteller/Audi_logo_klein.png";
+import vwLogo from "../img/logos_hersteller/VW_logo_klein.png";
+import skodaLogo from "../img/logos_hersteller/Skoda_logo_klein.png";
+import vwNfLogo from "../img/logos_hersteller/VW_NF_logo_klein.png";
+import fiatLogo from "../img/logos_hersteller/Fiat_logo_klein.png";
+import fiatNfLogo from "../img/logos_hersteller/Fiat_NF_logo_klein.jpg";
+import seatLogo from "../img/logos_hersteller/Seat_logo_klein.png";
 
 const Navbar = class extends React.Component {
   constructor(props) {
-    super(props)
+    super(props);
     this.state = {
       active: false,
-      navBarActiveClass: '',
-    }
+      navBarActiveClass: "",
+    };
   }
 
   toggleHamburger = () => {
@@ -23,28 +30,52 @@ const Navbar = class extends React.Component {
         // set the class in state for the navbar accordingly
         this.state.active
           ? this.setState({
-              navBarActiveClass: 'is-active',
+              navBarActiveClass: "is-active",
             })
           : this.setState({
-              navBarActiveClass: '',
-            })
+              navBarActiveClass: "",
+            });
       }
-    )
-  }
+    );
+  };
 
   render() {
     return (
-      <nav
-        className="navbar is-transparent"
-        role="navigation"
-        aria-label="main-navigation"
-      >
-        <div className="container">
-          <div className="navbar-brand">
-            <Link to="/" className="navbar-item" title="Logo">
-              <img src={logo} alt="Kaldi" style={{ width: '88px' }} />
+      <div className="navbar-wrapper box-shadow">
+        <div className="navbar-top container">
+          <div className="logo-wrapper">
+            <img src={logo} alt="Logo MAZ" />
+          </div>
+          <div className="hersteller-bar">
+            <Link to="/about" className="box-shadow">
+              <img src={vwLogo} alt="VW-Logo" />
             </Link>
+            <Link to="/about" className="box-shadow">
+              <img src={audiLogo} alt="Audi-Logo" />
+            </Link>
+            <Link to="/about" className="box-shadow">
+              <img src={skodaLogo} alt="Skoda-Logo" />
+            </Link>
+            <Link to="/about" className="box-shadow">
+              <img src={seatLogo} alt="Seat-Logo" />
+            </Link>
+            <Link to="/about" className="box-shadow">
+              <img src={vwNfLogo} alt="VW-Nutzfahrzeuge-Logo" />
+            </Link>
+            <Link to="/about" className="box-shadow">
+              <img src={fiatLogo} alt="Fiat-Logo" />
+            </Link>
+            <Link to="/about" className="box-shadow">
+              <img src={fiatNfLogo} alt="Fiat-Nutzfahrzeuge-Logo" />
+            </Link>
+          </div>
+        </div>
+        <nav className="navbar" role="navigation" aria-label="main-navigation">
+          <div className="container">
             {/* Hamburger menu */}
+            <div className="logo-wrapper logo-wrapper-desktop">
+              <img src={logo} alt="Logo MAZ" />
+            </div>
             <div
               className={`navbar-burger burger ${this.state.navBarActiveClass}`}
               data-target="navMenu"
@@ -54,45 +85,82 @@ const Navbar = class extends React.Component {
               <span />
               <span />
             </div>
-          </div>
-          <div
-            id="navMenu"
-            className={`navbar-menu ${this.state.navBarActiveClass}`}
-          >
-            <div className="navbar-start has-text-centered">
-              <Link className="navbar-item" to="/about">
-                About
-              </Link>
-              <Link className="navbar-item" to="/products">
-                Products
-              </Link>
-              <Link className="navbar-item" to="/blog">
-                Blog
-              </Link>
-              <Link className="navbar-item" to="/contact">
-                Contact
-              </Link>
-              <Link className="navbar-item" to="/contact/examples">
-                Form Examples
-              </Link>
-            </div>
-            <div className="navbar-end has-text-centered">
-              <a
-                className="navbar-item"
-                href="https://github.com/netlify-templates/gatsby-starter-netlify-cms"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                <span className="icon">
-                  <img src={github} alt="Github" />
-                </span>
-              </a>
-            </div>
-          </div>
-        </div>
-      </nav>
-    )
-  }
-}
+            <div
+              id="navMenu"
+              className={`navbar-menu ${this.state.navBarActiveClass}`}
+            >
+              <div className="navbar-start has-text-centered">
+                <div className="navbar-item has-dropdown is-hoverable">
+                  <a className="navbar-link">Angebote</a>
 
-export default Navbar
+                  <div className="navbar-dropdown">
+                    <Link className="navbar-item" to="/about">
+                      Fahrzeugangebote
+                    </Link>
+                    <Link className="navbar-item" to="/about">
+                      Nutzfahrzeuge
+                    </Link>
+                    <Link className="navbar-item" to="/about">
+                      Marken
+                    </Link>
+                    <Link className="navbar-item" to="/about">
+                      Zubehör-Prospekt
+                    </Link>
+                  </div>
+                </div>
+                <Link className="navbar-item" to="/about">
+                  MAZ Autorate
+                </Link>
+                <div className="navbar-item has-dropdown is-hoverable">
+                  <a className="navbar-link">Service & Werkstatt</a>
+
+                  <div className="navbar-dropdown">
+                    <Link className="navbar-item" to="/about">
+                      Werkstatttermin vereinbaren
+                    </Link>
+                    <Link className="navbar-item" to="/about">
+                      Probefahrt vereinbaren
+                    </Link>
+                    <Link className="navbar-item" to="/about">
+                      Serviceleistungen
+                    </Link>
+                    <Link className="navbar-item" to="/about">
+                      Zubehör-Prospekt
+                    </Link>
+                  </div>
+                </div>
+                <Link className="navbar-item" to="/blog">
+                  e-Mobilität
+                </Link>
+                <Link className="navbar-item" to="/contact">
+                  Geschäftskunden
+                </Link>
+                <Link className="navbar-item" to="/contact/examples">
+                  Karriere
+                </Link>
+                <div className="navbar-item has-dropdown is-hoverable">
+                  <a className="navbar-link">Über uns</a>
+
+                  <div className="navbar-dropdown">
+                    <Link className="navbar-item" to="/about">
+                      Feedback
+                    </Link>
+                    <Link className="navbar-item" to="/about">
+                      Standorte
+                    </Link>
+                    <Link className="navbar-item" to="/about">
+                      Kontakt
+                    </Link>
+                  </div>
+                </div>
+              </div>
+              <div className="navbar-end has-text-centered"></div>
+            </div>
+          </div>
+        </nav>
+      </div>
+    );
+  }
+};
+
+export default Navbar;
